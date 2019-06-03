9be3d39112b4227eaff958fbc72a826452cb845a
shar09 <sharathtelu9@gmail.com>
2019-06-03 18:38:38 -0500

    Functions are created to simplify the code and comments are added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 $(document).ready(function() 
-{   var randomNumber=0;
+{   
+    //Creating variables
+    var randomNumber=0;
     var crystalNumber1=0;
     var crystalNumber2=0;
     var crystalNumber3=0;
@@ -8,6 +10,8 @@ $(document).ready(function()
     var wins=0;
     var losses=0;
     
+    //Generates random numbers for each crystal and number to match
+    function numGenerate() {
     crystalNumber1 = Math.floor(Math.random() * 12)+1;
     console.log(crystalNumber1);
     crystalNumber2 = Math.floor(Math.random() * 12)+1;
@@ -18,86 +22,72 @@ $(document).ready(function()
     console.log(crystalNumber4);
     randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     $("#number-to-match").text(randomNumber);
+    }
+
+    numGenerate();
+    
+    //Resets crystal numbers, number to match and score
     function reset() 
     {
-        crystalNumber1 = Math.floor(Math.random() * 12)+1;
-        console.log(crystalNumber1);
-        crystalNumber2 = Math.floor(Math.random() * 12)+1;
-        console.log(crystalNumber2);
-        crystalNumber3 = Math.floor(Math.random() * 12)+1;
-        console.log(crystalNumber3);
-        crystalNumber4 = Math.floor(Math.random() * 12)+1;
-        console.log(crystalNumber4);
-        randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-        $("#number-to-match").text(randomNumber);
+        numGenerate();
         score=0;
         $("#target-number").text(score);
     };
-
+    
+    //Increments win counter and calls reset function
     function win() {
-            wins++;
-            $("#win-count").text(wins);
-            reset();
-            alert("you win");
+        wins++;
+        $("#win-count").text(wins);
+        reset();
+        alert("You Win!");
     }
     
+    //Incerements loss counter and calls reset function
     function lose() {
-            losses++
-            $("#loss-count").text(losses);
-            reset();
-            alert("you lose");  
+        losses++
+        $("#loss-count").text(losses);
+        reset();
+        alert("You Lose!");  
     }
-      
+    
+    //Compares number to match with score and calls win or lose function accordingly
+    function decider() {
+        if(randomNumber===score) {
+            win();
+        }
+ 
+        if(score>=randomNumber) {
+            lose();
+        }  
+    }
+    
+    //Value of crystal is added to score depending on what crystal is clicked and decider function is called
     $("#crystal-1").on("click", function() {
         $("#crystal-1").val(crystalNumber1);
         score=score+crystalNumber1;
         $("#target-number").text(score);
-        if(randomNumber===score) {
-           win();
-        }
-
-        if(score>=randomNumber) {
-           lose();
-        }  
+        decider();
     });
 
     $("#crystal-2").on("click", function() {
         $("#crystal-2").val(crystalNumber2);
         score=score+crystalNumber2;
         $("#target-number").text(score);
-        if(randomNumber===score) {
-            win();
-        }
-    
-        if(score>=randomNumber) {
-            lose();
-            }  
+        decider();
     });
         
     $("#crystal-3").on("click", function() {
         $("#crystal-3").val(crystalNumber3);
         score=score+crystalNumber3;
         $("#target-number").text(score);
-        if(randomNumber===score) {
-            win();
-        }
-        
-        if(score>=randomNumber) {
-            lose();
-            }  
+        decider(); 
     }); 
 
     $("#crystal-4").on("click", function() {  
         $("#crystal-4").val(crystalNumber4);
         score=score+crystalNumber4;
         $("#target-number").text(score);
-        if(randomNumber===score) {
-            win();
-        }
-            
-        if(score>=randomNumber) {
-            lose();
-        }  
+        decider();
     });
 });
             
